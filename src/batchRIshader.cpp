523e8ff0d0716f2851704cdb68f96aaac87689db
demotomohiro <gpuppur@gmail.com>
2017-08-12 21:22:15 +0900

    Replaced a part of print_gl_info() with GLUtil::print_context_info().

--- src/batchRIshader.cpp
+++ src/batchRIshader.cpp
@@ -16,6 +16,7 @@
 #include "frame_buffer_object.hpp"
 #include "image_file.hpp"
 #include "options.hpp"
+#include "GLUtil/contextUtil.hpp"
 
 using namespace std;
 
@@ -58,43 +59,7 @@ void init_gl()
 
 void print_gl_info()
 {
-	GLint contextProfileMask;
-	glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &contextProfileMask);
-	string contextProfileMaskStr;
-	if(contextProfileMask & GL_CONTEXT_CORE_PROFILE_BIT)
-		contextProfileMaskStr += "GL_CONTEXT_CORE_PROFILE_BIT ";
-	if(contextProfileMask & GL_CONTEXT_COMPATIBILITY_PROFILE_BIT)
-		contextProfileMaskStr += "CONTEXT_COMPATIBILITY_PROFILE_BIT ";
-
-	GLint contextFlags;
-	glGetIntegerv(GL_CONTEXT_FLAGS, &contextFlags);
-	string contextFlagsStr;
-	if(contextFlags & GL_CONTEXT_FLAG_DEBUG_BIT)
-		contextFlagsStr += "GL_CONTEXT_FLAG_DEBUG_BIT ";
-	if(contextFlags & GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT)
-		contextFlagsStr += "GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT ";
-
-	cout <<
-		"\nGL_VERSION:\n\t" <<
-			reinterpret_cast<const char*>(glGetString(GL_VERSION)) <<
-		"\nGL_SHADING_LANGUAGE_VERSION:\n\t" <<
-			reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION)) <<
-		"\nGL_RENDERER:\n\t" <<
-			reinterpret_cast<const char*>(glGetString(GL_RENDERER)) <<
-		"\nGL_VENDOR:\n\t" <<
-			reinterpret_cast<const char*>(glGetString(GL_VENDOR)) <<
-		"\nGL_CONTEXT_PROFILE_MASK:\n\t" <<
-			contextProfileMaskStr	<<
-		"\nGL_CONTEXT_FLAGS:\n\t" <<
-			contextFlagsStr <<
-		endl
-		;
-
-	GLint major_ver;
-	GLint minor_ver;
-	glGetIntegerv(GL_MAJOR_VERSION, &major_ver);
-	glGetIntegerv(GL_MINOR_VERSION, &minor_ver);
-	cout << "OpenGL version:" << major_ver << "." << minor_ver << endl;
+    GLUtil::print_context_info();
 
 	GLfloat point_size_range[2];
 	glGetFloatv(GL_POINT_SIZE_RANGE, point_size_range); 
